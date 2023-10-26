14ccf03d51164471ba171ee9d5717b27f27454d8
Jordan Lai <61778058+CrimsX@users.noreply.github.com>
2023-10-26 11:59:01 -0600

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -3,4 +3,5 @@
 curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py
 sudo python2.7 get-pip.py
 python2.7 -m pip install --upgrade setuptools
+pip2 install pipenv
 python2.7 -m pip install --upgrade --force-reinstall -r requirements.txt
